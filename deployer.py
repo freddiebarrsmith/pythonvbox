b34944571e87263d548524de702fc3ce49e9c6b1
root <root@debian>
2017-05-27 00:55:26 +0100

    vm starting script works

--- deployer.py
+++ deployer.py
@@ -1,6 +1,10 @@
 import virtualbox 
+import time
 vm = virtualbox.VirtualBox().find_machine('ubuntu') 
 session = vm.create_session() 
 s = virtualbox.Session() 
-vm.unlock_machine()
+print(session.state)
+time.sleep(5)
+session.unlock_machine()
+time.sleep(5)
 vm.launch_vm_process()
